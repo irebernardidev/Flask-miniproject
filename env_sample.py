30f61d7f80e26f9324982f4846ba45109fd86047
Ire <ireneand89@gmail.com>
2023-10-12 10:25:56 +0000

    Added env_sample

---
+++ env_sample.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "VL_G~<^9RczA]\"mYQ/4ADw&e@VXl<W")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser89@cluster0.1piqwhn.mongodb.net/task_manager?retryWrites=true&w=majority&appName=AtlasApp")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
